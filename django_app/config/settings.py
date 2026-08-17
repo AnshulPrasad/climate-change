d3d5535d6be5a3f0dfb8dacde1533c2afb65340f
Anshul Prasad <anshulprasad5440.com>
2026-08-17 10:06:59 +0530

    For security purpose, lets hide secrete key of django.

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9983-&^h1vj&61_3_nu*mwmp5o#ai_llf5og00q$fwf#tr&)cy'
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-key-for-local-dev-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
