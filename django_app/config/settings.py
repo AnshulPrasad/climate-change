2c3866b6b9f994f351a514471a35751dfab2ca95
Anshul Prasad <anshulprasad5440.com>
2026-09-01 18:22:58 +0530

    added useful comments

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hf_space',
+    'hf_space', # added our app
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware', # it is giving error on hugging face space
 ]
 
 ROOT_URLCONF = 'config.urls'
